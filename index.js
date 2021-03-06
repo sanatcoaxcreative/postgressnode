dc87ddf51f29af74f0838f4bb150d152db114b4c
sanat <sanat@coaxcreative.com>
2021-03-06 12:25:39 +0530

    Chnagess

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const {Client} = require('pg');
 const app = require('express')();
+const client = new Client({
+    connectionString:'postgres://gmspmkgjlozzzb:94fcaad47af5d1f3f87a391405bb97969968befdf67918c68ed59d332fdad651@ec2-54-205-61-191.compute-1.amazonaws.com:5432/d3jhvsu54b60vf'
+})
+try {
+    client.connect().then(()=>{
+        console.log('connection wth postgress!!');
+    }).catch((e)=>console.log('hello',e))
+} catch (error) {
+    console.log('failed to connect pg!!!')
+}
 app.get('/', (req, res)=>{
     res.send({
         hello:'node js with postgres!'
