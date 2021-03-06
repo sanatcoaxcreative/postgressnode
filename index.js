7947aad8e39e98a5298c0bd3b02093b3b45ad605
sanat <sanat@coaxcreative.com>
2021-03-06 12:29:59 +0530

    Chnagess

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const app = require('express')();
 const client = new Client({
     connectionString:'postgres://gmspmkgjlozzzb:94fcaad47af5d1f3f87a391405bb97969968befdf67918c68ed59d332fdad651@ec2-54-205-61-191.compute-1.amazonaws.com:5432/d3jhvsu54b60vf'
 })
-try {
-    client.connect().then(()=>{
-        console.log('connection wth postgress!!');
-    }).catch((e)=>console.log('hello',e))
-} catch (error) {
-    console.log('failed to connect pg!!!')
-}
+
 app.get('/', (req, res)=>{
-    res.send({
-        hello:'node js with postgre sql!'
-    })
+    client.connect().then((data)=>{
+        console.log('connection wth postgress!!');
+        res.send({
+            hello:'node js with postgre sql!',
+            data:data
+        })
+    }).catch((e)=>res.send(e))
+    
 })
 app.listen(process.env.PORT||5000, ()=>{
     console.log('Listening to server!!!')
